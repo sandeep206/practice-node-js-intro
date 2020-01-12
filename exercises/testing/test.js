80fd184984a70fbb13249ec62916bb88c88cdf6a
Sasidharan, Sandeep (ITCDCC) - KLM <Sandeep.Sasidharan@klm.com>
2020-01-12 13:44:52 +0100

    completed excercise

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -3,6 +3,6 @@ const { fixID } = require('./users');
 describe('users', () => {
   test('should return numerical', () => {
     const id = "500";
-      expect(fixID(id)).toBe(500);
+      expect(fixID(id)).toBe(500); 
   })
 })
